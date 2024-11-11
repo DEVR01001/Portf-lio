3e00d6c5c3abe04e5c64ac628c5ee207d6fce998
DEVR01001 <rafael.dev01001@gmail.com>
2024-11-10 22:30:35 -0400

    sdfgfds

---
+++ portiflolio/js/ani_paper.js
@@ -0,0 +1,7 @@
+const btn= document.getElementById('link_email');
+const paper = document.getElementById('img_paper');
+
+
+btn.addEventListener('click', ()=>{
+    paper.classList.add('active_ani_paper');
+});
