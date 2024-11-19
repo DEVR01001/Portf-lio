3e59f8c67d09e164478ac42db0a0e617c81ef06a
DEVR01001 <rafael.dev01001@gmail.com>
2024-11-19 11:13:19 -0400

    dfghj

--- portiflolio/js/animacao.js
+++ portiflolio/js/animacao.js
@@ -13,3 +13,17 @@ const elementos = document.querySelectorAll('.hidden')
 
 
 elementos.forEach((elemeto) => Observer.observe(elemeto))
+
+
+
+
+$(document).ready(function () {
+    $("html").niceScroll();
+});
+
+// For window
+window.scrollTo({
+    left: x,
+    top: y,
+    behavior: 'smooth'
+});
