49e8cecc6dfd420808d1a70733a2ece47625586d
DEVR01001 <rafael.dev01001@gmail.com>
2024-11-20 20:12:36 -0400

    90%

--- portiflolio/js/btn-voltar.js
+++ portiflolio/js/btn-voltar.js
@@ -1,10 +1,5 @@
 const btnVoltar = document.getElementById('voltar')
 
-
-
-
-
-
 // VOLTA PARA O TOPO NUM EVENTO DE CLICK
 
 btnVoltar.addEventListener('click', ()=>{
@@ -17,3 +12,38 @@ btnVoltar.addEventListener('click', ()=>{
 })
 
 
+
+
+// LIGHTMODE E DARKMODE
+// Obtem o estado do tema armazenado no localStorage
+let lightmode = localStorage.getItem('lightmode');
+
+const icon_theme = document.getElementById('icon_theme');
+
+// Função para ativar o modo claro
+const enableLightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+};
+
+// Função para desativar o modo claro
+const disableLightMode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', 'inactive');
+};
+
+// Alterna o tema ao clicar no botão
+icon_theme.addEventListener('click', () => {
+    if (lightmode === 'active') {
+        disableLightMode();
+        lightmode = 'inactive';
+    } else {
+        enableLightmode();
+        lightmode = 'active';
+    }
+});
+
+// Verifica o estado inicial do tema
+if (lightmode === 'active') {
+    enableLightmode();
+}
