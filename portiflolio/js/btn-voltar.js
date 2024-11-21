1f8edde6b604d42beda9296af300bd3a3606eb42
DEVR01001 <rafael.dev01001@gmail.com>
2024-11-20 21:40:11 -0400

    90%

--- portiflolio/js/btn-voltar.js
+++ portiflolio/js/btn-voltar.js
@@ -14,25 +14,23 @@ btnVoltar.addEventListener('click', ()=>{
 
 
 
-// LIGHTMODE E DARKMODE
-// Obtem o estado do tema armazenado no localStorage
+
 let lightmode = localStorage.getItem('lightmode');
 
 const icon_theme = document.getElementById('icon_theme');
 
-// Função para ativar o modo claro
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
 };
 
-// Função para desativar o modo claro
+
 const disableLightMode = () => {
     document.body.classList.remove('lightmode');
     localStorage.setItem('lightmode', 'inactive');
 };
 
-// Alterna o tema ao clicar no botão
+
 icon_theme.addEventListener('click', () => {
     if (lightmode === 'active') {
         disableLightMode();
@@ -43,7 +41,16 @@ icon_theme.addEventListener('click', () => {
     }
 });
 
-// Verifica o estado inicial do tema
+
 if (lightmode === 'active') {
     enableLightmode();
 }
+
+
+
+
+
+
+
+
+
